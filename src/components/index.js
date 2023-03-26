c2b5f0c2b0df05003f56d6bc30e4b3e3ae12a4c1
mehak0903 <vmehak0903@gmail.com>
2023-03-26 18:05:11 +0530

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import article from './article/article';
+import brand from './brand/brand';
+import cta from './cta/cta';
+import feature from './feature/feature';
+import navbar from './navbar/navbar';
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css';
+
+const navbar=()=>{
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+
+export default navbar
